daadf1e4c24b090bf9f762fb16d0c63e5cfe603c
Sumon Hossain <sumonengr.96@gmail.com>
2025-07-18 12:41:47 +0600

    logout

--- src/app/layouts/sidebar/sidebar.ts
+++ src/app/layouts/sidebar/sidebar.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
   currentYear: number = new Date().getFullYear();
+
+  router = inject(Router);
+
+  logout() {
+    localStorage.removeItem('leaveUser');
+    this.router.navigateByUrl('/login');
+  }
 }
